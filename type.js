594adbb8f3a4660a4acc7f72d56b5f3b0fa2d055
Alex R <anfurny22@gmail.com>
2012-05-17 01:26:51 -0700

    fixed bug that caused parameters referenced by variable name to be ignored

--- type.js
+++ type.js
@@ -4,14 +4,135 @@
  * Type checking for javascript
  * @author Alex Rohde
  * @created 2012
- */
-var TYPE = {};
+ */ 
 
 /*  Potential ambiguities:
  
  Though the following are all instanceof object in javascript, this library does not trigger true for TYPE.object (date, html element, array [], function, )
 	if you want to allow any of these use simply Object , otherwise use TYPE.object , or TYPE.map
  */
+
+
+
+/**
+ * 
+ * @todo: basic error validation
+ * @todo: refactoring described below.
+ * @todo: Enhance expected error message. If expected was an array of possibilities, display it well.
+ * @todo: enhance error message to include line numbers
+ * // @todo: decide if setting a type, but no default, should emit an error
+ * @todo: refactor code to unify class constants and tests array 
+ */
+
+TYPE = function(info) {
+	var x, tmp, next = [], failed = false, checks = {};
+	var passed = Array.prototype.slice.call(TYPE.caller.arguments);
+	/*splice makes it a real array */
+	var expected = TYPE.caller.toString();
+	/* The list of actual function parameters in the declaration */
+	expected = expected.split(")")[0].split("(")[1].split(" ").join("").split(",");
+	
+
+	var type_key = 'type', value_key = 'val', default_key = "def";
+	/* If the coder uses an assoc object, these valid_keys are the valid properties/keys */
+	var valid_keys = [type_key, value_key, default_key];
+
+	if(TYPE.getType(info) !== "Array") {
+		TYPE.raiseError("TYPE: CheckParam ITSELF wasn't provided an array. Please see documentation.");
+	}
+	if(info.length < 1) {/*Empty Array Passed. Okay */
+		return;
+	}
+	if(TYPE.getType(info[0]) === "Array") {/* Info a Numeric array & Not an assoc array? : make it one */
+		for( x = 0; x < info.length; x++) {
+			if((TYPE.getType(info[x]) !== "Array") || (info[x].length < 1))/*For some reason this function was provided a malformed array */
+				return TYPE.raiseError("TYPE: Could not understand...");
+			tmp = {	'var' : info[x][0] };
+			
+			if(info[x].length > 1)
+				tmp.type = info[x][1];
+			if(info[x].length > 2)
+				tmp[default_key] = info[x][2];
+			info[x] = tmp;
+			checks[info[x]['var']] = tmp;
+		}
+	} else if (TYPE.getType(info[0]) === "Object") {/* Info an assoc array? : check validity */
+		for( x = 0; x < info.length; x++) {
+			if((TYPE.getType(info[x]) !== "Object") || (!info[x].hasOwnProperty[default_key]))/*For some reason this function was provided a malformed array */
+				return TYPE.raiseError("TYPE: Could not understand...");
+			if (info[x].hasOwnProperty('var'))
+				checks[info['var']] = info[x];
+			else
+				checks[expected[x]] = info[x];	/* In an assoc array, make the var element optional */
+			}
+
+	} else {
+		return TYPE.raiseError("TYPE: Malformed data given. It must be an array of arrays/objects");
+	}
+
+	for( x = 0; x < expected.length; x++) {/* For each actual parameter */
+	
+		if (checks.hasOwnProperty(expected[x]))
+		  if(passed[x] !== null) {/* An actual parameter was provided already */
+			var type = (checks[expected[x]][type_key]);
+			var okay = false, def = [];
+			var value = passed[x];
+			
+			if( typeof (checks[expected[x]][type_key]) !== null) {/* Typeof param was included */
+				
+				if (TYPE.getType(type) !== "Array") { /* always treat as array*/
+					type = [type];
+				}
+
+				if (checks[expected[x]].hasOwnProperty(default_key) ) {
+					def = checks[expected[x]][default_key];
+				}
+						
+				for (y = 0; (y < type.length) && (!okay); y++) {
+					okay |= TYPE.validateParam(type[y], value, def);
+				}		
+					
+				if(!okay) {
+					var desc = "";
+					if (value && value.toString) 
+						desc = value.toString();
+						
+					TYPE.raiseError("TYPE. Type Checking Assertion Failed on Param #" + (x + 1) + " \"" + expected[x] + "\". Recevied: " + desc + ", expected " + type.toString());
+				/* @todo provide function information like above, perhaps in raiseerror */
+				}
+			}
+			/* Okay, we passed validation */
+			next.push(value);
+			/* Use it */
+		} else {/* Otherwise, generate a default */
+			if(checks[expected[x]][default_key] == TYPE.Err) {/* Not an optional parameter */
+				TYPE.raiseError("TYPE: Required parameter missing P(" + (x + 1) + ")");
+				failed = true;
+			}
+
+			if( !checks[expected[x]].hasOwnProperty(default_key)) {
+				/* No default specified, and no parameter specified... so null */
+				next.push(null);
+			} else {
+				next.push(checks[expected[x]][default_key]);
+			}
+		}
+	}
+	
+	for (ind in checks) {
+		if (checks.hasOwnProperty(ind))
+			if ( expected.indexOf(ind) == -1) {
+				TYPE.raiseError("TYPE: Check provided for unknown variable. Function doesn't have a variable '" + ind + "'");
+				failed = true;
+			}
+	} 
+	
+	if((passed.join(",") != next.join(",")) && (!failed)) {
+		/* Call again with correct default values */
+		return TYPE.caller.apply(this, next);
+	}
+	return false;
+};
 
 /**
  * Class Constants
@@ -49,10 +170,11 @@ TYPE.getType = function(o) {
 /**
  * raiseError
  * 
- * You can connect this function to your desired error-handler method
+ * You can connect this function to your desired error-handler method. It should return falsey.
  */
 TYPE.raiseError = function(a) {
 	alert(a);
+	return false;
 };
 
 /**
@@ -96,110 +218,6 @@ TYPE.runTest = function () {
 		alert("Fails to match on " + [x] + " with " + matches.join(","));
 	}
 }
-
-/**
- * @todo: refactoring described below.
- * @todo: Enhance expected error message. If expected was an array of possibilities, display it well.
- * @todo: enhance error message to include line numbers
- * // @todo: decide if setting a type, but no default, should emit an error
- * @todo: refactor code to unify class constants and tests array 
- */
-
-TYPE.run = function(info) {
-	var x, tmp, next = [], failed = false;
-	var passed = Array.prototype.slice.call(TYPE.run.caller.arguments);
-	/*splice makes it a real array */
-	var expected = TYPE.run.caller.toString();
-	/* The list of actual function parameters in the declaration */
-	expected = expected.split(")")[0].split("(")[1].split(" ").join("").split(",");
-	
-
-	var type_key = 'type', value_key = 'val', default_key = "def";
-	/* If the coder uses an assoc object, these valid_keys are the valid properties/keys */
-	var valid_keys = [type_key, value_key, default_key];
-
-	if(TYPE.getType(info) !== "Array") {
-		TYPE.raiseError("TYPE: CheckParam ITSELF wasn't provided an array. Please see documentation.");
-	}
-	if(info.length < 1) {/*Empty Array Passed. Okay */
-		return;
-	}
-	if(TYPE.getType(info[0]) === "Array") {/* Info a Numeric array & Not an assoc array? : make it one */
-		for( x = 0; x < info.length; x++) {
-			if((TYPE.getType(info[x]) !== "Array") || (info[x].length < 1))/*For some reason this function was provided a malformed array */
-				return TYPE.raiseError("TYPE: Could not understand...");
-			tmp = {
-				'var' : info[x][0]
-			};
-			if(info[x].length > 1)
-				tmp.type = info[x][1];
-			if(info[x].length > 2)
-				tmp[default_key] = info[x][2];
-			info[x] = tmp;
-		}
-	} else if (TYPE.getType(info[0]) === "Object") {/* Info a Numeric array & Not an assoc array? : make it one */
-		for( x = 0; x < info.length; x++) {
-			if((TYPE.getType(info[x]) !== "Object") || (!info[x].hasOwnProperty[default_key]))/*For some reason this function was provided a malformed array */
-				return TYPE.raiseError("TYPE: Could not understand...");		
-			}
-	} else {
-		return TYPE.raiseError("TYPE: Malformed data given. It must be an array of arrays/objects");
-	}
-
-	for( x = 0; x < expected.length; x++) {/* For each actual parameter */
-
-		if(passed[x] !== null) {/* An actual parameter was provided already */
-			var type = (info[x][type_key]);
-			var okay = false, def = [];
-			var value = passed[x];
-			
-			if( typeof (info[x][type_key]) !== null) {/* Typeof param was included */
-				
-				if (TYPE.getType(type) !== "Array") { /* always treat as array*/
-					type = [type];
-				}
-
-				if (info[x].hasOwnProperty(default_key) ) {
-					def = info[x][default_key];
-				}
-						
-				for (y = 0; (y < type.length) && (!okay); y++) {
-					okay |= TYPE.validateParam(type[y], value, def);
-				}		
-					
-				if(!okay) {
-					var desc = "";
-					if (value && value.toString) 
-						desc = value.toString();
-						
-					TYPE.raiseError("TYPE. Type Checking Assertion Failed on Param #" + (x + 1) + " \"" + expected[x] + "\". Recevied: " + desc + ", expected " + type.toString());
-				/* @todo provide function information like above, perhaps in raiseerror */
-				}
-			}
-			/* Okay, we passed validation */
-			next.push(value);
-			/* Use it */
-		} else {/* Otherwise, generate a default */
-			if(info[x][default_key] == TYPE.Err) {/* Not an optional parameter */
-				TYPE.raiseError("TYPE: Required parameter missing P(" + (x + 1) + ")");
-				failed = true;
-			}
-
-			if( !info[x].hasOwnProperty(default_key)) {
-				/* No default specified, and no parameter specified... so null */
-				next.push(null);
-			} else {
-				next.push(info[x][default_key]);
-			}
-		}
-
-	}
-	if((passed.join(",") != next.join(",")) && (!failed)) {
-		/* Call again with correct default values */
-		return TYPE.run.caller.apply(this, next);
-	}
-	return false;
-};
 
 /**
  * Does the actual validation of a single type and a value
